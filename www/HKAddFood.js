863d93ec3b7dbb552c35781946f16c16044d729f
Aaron Clifford <egoant@hotmail.com>
2015-06-05 00:21:40 -0600

    Removed "define" wrapper and added checkAuthStatus

--- www/HKAddFood.js
+++ www/HKAddFood.js
@@ -1,23 +1,11 @@
-cordova.define("com.egoant.plugins.hkaddfood.HKAddFood", function(require, exports, module) { function HKAddFood() {
-}
-
 HKAddFood.prototype.available = function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "HKAddFood", "available", []);
+};
+
+HKAddFood.prototype.checkAuthStatus = function (options, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "HKAddFood", "checkAuthStatus", [options]);
 };
 
  HKAddFood.prototype.requestAuthorization = function (options, successCallback, errorCallback) {
      cordova.exec(successCallback, errorCallback, "HKAddFood", "requestAuthorization", [options]);
 };
-                                                                                             
-HKAddFood.install = function () {
-     if (!window.plugins) {
-         window.plugins = {};
-     }
-
-     window.plugins.hkaddfood = new HKAddFood();
-    return window.plugins.hkaddfood;
- };
-
-cordova.addConstructor(HKAddFood.install);
-
-});
