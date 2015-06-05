446d81ddd68f63aa6b6ce34bd12e5d2bec7c87c2
Aaron Clifford <egoant@hotmail.com>
2015-06-04 22:53:06 -0600

    Added checkAuthStatus

--- src/ios/HKAddFood.m
+++ src/ios/HKAddFood.m
@@ -34,6 +34,30 @@ CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK 
 [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
+- (void) checkAuthStatus:(CDVInvokedUrlCommand*)command {
+// Note for doc, if status = denied, prompt user to go to settings or the Health app
+NSMutableDictionary *args = [command.arguments objectAtIndex:0];
+NSString *checkType = [args objectForKey:HKPluginKeyType];
+
+HKObjectType *type = [self getHKObjectType:checkType];
+if (type == nil) {
+CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"type is an invalid value"];
+[self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+} else {
+HKAuthorizationStatus status = [self.healthStore authorizationStatusForType:type];
+NSString *result;
+if (status == HKAuthorizationStatusNotDetermined) {
+result = @"undetermined";
+} else if (status == HKAuthorizationStatusSharingDenied) {
+result = @"denied";
+} else if (status == HKAuthorizationStatusSharingAuthorized) {
+result = @"authorized";
+}
+CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+}
+
 - (void) requestAuthorization:(CDVInvokedUrlCommand*)command {
 NSMutableDictionary *args = [command.arguments objectAtIndex:0];
 }
