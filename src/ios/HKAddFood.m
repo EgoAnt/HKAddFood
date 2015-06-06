499359ab9be0fcd89a72e6a3a449b93391de7e22
Aaron Clifford <egoant@hotmail.com>
2015-06-06 09:31:44 -0600

    Added in all helpers.

--- src/ios/HKAddFood.m
+++ src/ios/HKAddFood.m
@@ -67,43 +67,184 @@ static NSString *const HKPluginKeyUUID = @"UUID";
 	double calDouble = [foodCalories doubleValue];
 
 	HKQuantityType *quantityType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDietaryCalories];
+	HKQuantity *quantity = [HKQuantity quantityWithUnit:[HKUnit kiloCalorieUnit] doubleValue:calDouble];
+
+	NSDate *objDate = [NSDate date];
+	NSDictionary *metaData = @{HKMetadataKeyFoodType:foodName};
+
+	HKQuantityType *foodItemSample =
+	[HKQuantitySample quantitySampleWithType:quantityType
+	quantity:quantity
+	startDate:objDate
+	endDate:objDate
+	metadata:metaData]
 
 
-
+	[self.healthStore saveObject:foodItemSample withCompletion:^(BOOL success, NSError *error){
+		if(success){
+			result = @"saved";
+			CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+		}else{
+			result = @"not saved";
+			CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+		}
+	}]
 }
 
 - (void) requestAuthorization:(CDVInvokedUrlCommand*)command {
 	NSMutableDictionary *args = [command.arguments objectAtIndex:0];
 }
 
+#pragma mark - helper methods
+- (HKUnit*) getUnit:(NSString*) type : (NSString*) expected {
+  HKUnit *localUnit;
+  @try {
+    localUnit = [HKUnit unitFromString:type];
+    if ([[[localUnit class] description] isEqualToString:expected]) {
+      return localUnit;
+    } else {
+      return nil;
+    }
+  }
+  @catch(NSException *e) {
+    return nil;
+  }
+}
+
 - (HKObjectType*) getHKObjectType:(NSString*) elem {
-	HKObjectType *type = [HKObjectType quantityTypeForIdentifier:elem];
-	if (type == nil) {
-		type = [HKObjectType characteristicTypeForIdentifier:elem];
-	}
-	if (type == nil){
-		type = [self getHKSampleType:elem];
-	}
-	return type;
+  HKObjectType *type = [HKObjectType quantityTypeForIdentifier:elem];
+  if (type == nil) {
+    type = [HKObjectType characteristicTypeForIdentifier:elem];
+  }
+  if (type == nil){
+    type = [self getHKSampleType:elem];
+  }
+  return type;
 }
 
 - (HKQuantityType*) getHKQuantityType:(NSString*) elem {
-	HKQuantityType *type = [HKQuantityType quantityTypeForIdentifier:elem];
-	return type;
+  HKQuantityType *type = [HKQuantityType quantityTypeForIdentifier:elem];
+  return type;
 }
 
 - (HKSampleType*) getHKSampleType:(NSString*) elem {
-    HKSampleType *type = [HKObjectType quantityTypeForIdentifier:elem];
-    if (type == nil) {
-		type = [HKObjectType categoryTypeForIdentifier:elem];
-    }
-    if (type == nil) {
-		type = [HKObjectType quantityTypeForIdentifier:elem];
-    }
-    if (type == nil) {
-		type = [HKObjectType correlationTypeForIdentifier:elem];
-    }
-
-    return type;
+  HKSampleType *type = [HKObjectType quantityTypeForIdentifier:elem];
+  if (type == nil) {
+    type = [HKObjectType categoryTypeForIdentifier:elem];
+  }
+  if (type == nil) {
+    type = [HKObjectType quantityTypeForIdentifier:elem];
+  }
+  if (type == nil) {
+    type = [HKObjectType correlationTypeForIdentifier:elem];
+  }
+  if (type == nil && [elem isEqualToString:@"workoutType"]) {
+    type = [HKObjectType workoutType];
+  }
+  return type;
 }
+
+//Helper to parse out a quantity sample from a dictionary and perform error checking
+- (HKQuantitySample*) loadHKQuantitySampleFromInputDictionary:(NSDictionary*) inputDictionary error:(NSError**) error {
+  //Load quantity sample from args to command
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeyStartDate error:error]) return nil;
+  NSDate *startDate = [NSDate dateWithTimeIntervalSince1970:[[inputDictionary objectForKey:HKPluginKeyStartDate] longValue]];
+  
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeyEndDate error:error]) return nil;
+  NSDate *endDate = [NSDate dateWithTimeIntervalSince1970:[[inputDictionary objectForKey:HKPluginKeyEndDate] longValue]];
+  
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeySampleType error:error]) return nil;
+  NSString *sampleTypeString = [inputDictionary objectForKey:HKPluginKeySampleType];
+  
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeyUnit error:error]) return nil;
+  NSString *unitString = [inputDictionary objectForKey:HKPluginKeyUnit];
+  
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeyAmount error:error]) return nil;
+  double value = [[inputDictionary objectForKey:HKPluginKeyAmount] doubleValue];
+  
+  //Load optional metadata key
+  NSDictionary* metadata = [inputDictionary objectForKey:HKPluginKeyMetadata];
+  if (metadata == nil)
+    metadata = @{};
+  
+  return [self getHKQuantitySampleWithStartDate:startDate endDate:endDate sampleTypeString:sampleTypeString unitTypeString:unitString value:value metadata:metadata error:error];
+}
+
+//Helper to parse out a correlation from a dictionary and perform error checking
+- (HKCorrelation*) loadHKCorrelationFromInputDictionary:(NSDictionary*) inputDictionary error:(NSError**) error {
+  //Load correlation from args to command
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeyStartDate error:error]) return nil;
+  NSDate *startDate = [NSDate dateWithTimeIntervalSince1970:[[inputDictionary objectForKey:HKPluginKeyStartDate] longValue]];
+  
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeyEndDate error:error]) return nil;
+  NSDate *endDate = [NSDate dateWithTimeIntervalSince1970:[[inputDictionary objectForKey:HKPluginKeyEndDate] longValue]];
+  
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeyCorrelationType error:error]) return nil;
+  NSString *correlationTypeString = [inputDictionary objectForKey:HKPluginKeyCorrelationType];
+  
+  if (![self inputDictionary:inputDictionary hasRequiredKey:HKPluginKeyObjects error:error]) return nil;
+  NSArray* objectDictionaries = [inputDictionary objectForKey:HKPluginKeyObjects];
+  
+  NSMutableSet* objects = [NSMutableSet set];
+  for (NSDictionary* objectDictionary in objectDictionaries) {
+    HKQuantitySample* sample = [self loadHKQuantitySampleFromInputDictionary:objectDictionary error:error];
+    if (sample == nil)
+      return nil;
+    [objects addObject:sample];
+  }
+  NSDictionary *metadata = [inputDictionary objectForKey:HKPluginKeyMetadata];
+  if (metadata == nil)
+    metadata = @{};
+  return [self getHKCorrelationWithStartDate:startDate endDate:endDate correlationTypeString:correlationTypeString objects:objects metadata:metadata error:error];
+}
+
+//Helper to isolate error checking on inputs for plugin
+-(BOOL) inputDictionary:(NSDictionary*) inputDictionary hasRequiredKey:(NSString*) key error:(NSError**) error {
+  if ([inputDictionary objectForKey:key] == nil) {
+    *error = [NSError errorWithDomain:HKPluginError code:0 userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat:@"required value -%@- was missing from dictionary %@",key,[inputDictionary description]]}];
+    return false;
+  }
+  return true;
+}
+
+// Helper to handle the functionality with HealthKit to get a quantity sample
+- (HKQuantitySample*) getHKQuantitySampleWithStartDate:(NSDate*) startDate endDate:(NSDate*) endDate sampleTypeString:(NSString*) sampleTypeString unitTypeString:(NSString*) unitTypeString value:(double) value metadata:(NSDictionary*) metadata error:(NSError**) error {
+  HKQuantityType *type = [self getHKQuantityType:sampleTypeString];
+  if (type==nil) {
+    *error = [NSError errorWithDomain:HKPluginError code:0 userInfo:@{NSLocalizedDescriptionKey:@"quantity type string was invalid"}];
+    return nil;
+  }
+  HKUnit *unit;
+  @try {
+    unit = unitTypeString!=nil ? [HKUnit unitFromString:unitTypeString] : nil;
+    if (unit==nil) {
+      *error = [NSError errorWithDomain:HKPluginError code:0 userInfo:@{NSLocalizedDescriptionKey:@"unit was invalid"}];
+      return nil;
+    }
+  }
+  @catch(NSException *e) {
+    *error = [NSError errorWithDomain:HKPluginError code:0 userInfo:@{NSLocalizedDescriptionKey:@"unit was invalid"}];
+    return nil;
+  }
+  HKQuantity *quantity = [HKQuantity quantityWithUnit:unit doubleValue:value];
+  if (![quantity isCompatibleWithUnit:unit]) {
+    *error = [NSError errorWithDomain:HKPluginError code:0 userInfo:@{NSLocalizedDescriptionKey:@"unit was not compatible with quantity"}];
+    return nil;
+  }
+  
+  return [HKQuantitySample quantitySampleWithType:type quantity:quantity startDate:startDate endDate:endDate metadata:metadata];
+}
+
+- (HKCorrelation*) getHKCorrelationWithStartDate:(NSDate*) startDate endDate:(NSDate*) endDate correlationTypeString:(NSString*) correlationTypeString objects:(NSSet*) objects metadata:(NSDictionary*) metadata error:(NSError**) error {
+  NSLog(@"correlation type is %@",HKCorrelationTypeIdentifierBloodPressure);
+  HKCorrelationType *correlationType = [HKCorrelationType correlationTypeForIdentifier:correlationTypeString];
+  if (correlationType == nil) {
+    *error = [NSError errorWithDomain:HKPluginError code:0 userInfo:@{NSLocalizedDescriptionKey:@"correlation type string was invalid"}];
+    return nil;
+  }
+  return [HKCorrelation correlationWithType:correlationType startDate:startDate endDate:endDate objects:objects metadata:metadata];
+}
+
 @end
