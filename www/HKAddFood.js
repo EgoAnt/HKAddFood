566db6cde3873e97f611c9c61de00edb587fdae1
Aaron Clifford <egoant@hotmail.com>
2015-06-08 08:49:36 -0600

    Cleaned up the mess in my Javascript file.

--- www/HKAddFood.js
+++ www/HKAddFood.js
@@ -10,8 +10,8 @@ HKAddFood.prototype.checkAuthStatus = function (options, successCallback, errorC
     cordova.exec(successCallback, errorCallback, "HKAddFood", "checkAuthStatus", [options]);
 };
 
- HKAddFood.prototype.requestAuthorization = function (options, successCallback, errorCallback) {
-     cordova.exec(successCallback, errorCallback, "HKAddFood", "requestAuthorization", [options]);
+HKAddFood.prototype.requestAuthorization = function (options, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "HKAddFood", "requestAuthorization", [options]);
 };
 
 HKAddFood.prototype.saveFoodItemCalories = function (options, successCallback, errorCallback) {
@@ -24,76 +24,75 @@ HKAddFood.prototype.saveFoodItemFatTotal = function (options, successCallback, e
 
 HKAddFood.prototype.saveFoodItemCalcium = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemCalcium", [options]); };
 
+HKAddFood.prototype.saveFoodItemBiotin = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemBiotin", [options]); };
 
-HKAddFood.prototype.saveFoodItemBiotin = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemBiotin", [options]; };
+HKAddFood.prototype.saveFoodItemCaffeine = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemCaffeine", [options]); };
 
-HKAddFood.prototype.saveFoodItemCaffeine = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemCaffeine", [options]; };
+HKAddFood.prototype.saveFoodItemCarbohydrates = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemCarbohydrates", [options]); };
 
-HKAddFood.prototype.saveFoodItemCarbohydrates = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemCarbohydrates", [options]; };
+HKAddFood.prototype.saveFoodItemChloride = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemChloride", [options]); };
 
-HKAddFood.prototype.saveFoodItemChloride = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemChloride", [options]; };
+HKAddFood.prototype.saveFoodItemCholesterol = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemCholesterol", [options]); };
 
-HKAddFood.prototype.saveFoodItemCholesterol = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemCholesterol", [options]; };
+HKAddFood.prototype.saveFoodItemChromium = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemChromium", [options]); };
 
-HKAddFood.prototype.saveFoodItemChromium = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemChromium", [options]; };
+HKAddFood.prototype.saveFoodItemCopper = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemCopper", [options]); };
 
-HKAddFood.prototype.saveFoodItemCopper = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemCopper", [options]; };
+HKAddFood.prototype.saveFoodItemFatMonounsaturated = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemFatMonounsaturated", [options]); };
 
-HKAddFood.prototype.saveFoodItemFatMonounsaturated = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemFatMonounsaturated", [options]; };
+HKAddFood.prototype.saveFoodItemFatPolyunsaturated = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemFatPolyunsaturated", [options]); };
 
-HKAddFood.prototype.saveFoodItemFatPolyunsaturated = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemFatPolyunsaturated", [options]; };
+HKAddFood.prototype.saveFoodItemFatSaturated = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemFatSaturated", [options]); };
 
-HKAddFood.prototype.saveFoodItemFatSaturated = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemFatSaturated", [options]; };
+HKAddFood.prototype.saveFoodItemFiber = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemFiber", [options]); };
 
-HKAddFood.prototype.saveFoodItemFiber = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemFiber", [options]; };
+HKAddFood.prototype.saveFoodItemFolate = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemFolate", [options]); };
 
-HKAddFood.prototype.saveFoodItemFolate = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemFolate", [options]; };
+HKAddFood.prototype.saveFoodItemIodine = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemIodine", [options]); };
 
-HKAddFood.prototype.saveFoodItemIodine = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemIodine", [options]; };
+HKAddFood.prototype.saveFoodItemIron = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemIron", [options]); };
 
-HKAddFood.prototype.saveFoodItemIron = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemIron", [options]; };
+HKAddFood.prototype.saveFoodItemMagnesium = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemMagnesium", [options]); };
 
-HKAddFood.prototype.saveFoodItemMagnesium = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemMagnesium", [options]; };
+HKAddFood.prototype.saveFoodItemManganese = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemManganese", [options]); };
 
-HKAddFood.prototype.saveFoodItemManganese = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemManganese", [options]; };
+HKAddFood.prototype.saveFoodItemMolybdenum = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemMolybdenum", [options]); };
 
-HKAddFood.prototype.saveFoodItemMolybdenum = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemMolybdenum", [options]; };
+HKAddFood.prototype.saveFoodItemNiacin = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemNiacin", [options]); };
 
-HKAddFood.prototype.saveFoodItemNiacin = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemNiacin", [options]; };
+HKAddFood.prototype.saveFoodItemPantothenicAcid = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemPantothenicAcid", [options]); };
 
-HKAddFood.prototype.saveFoodItemPantothenicAcid = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemPantothenicAcid", [options]; };
+HKAddFood.prototype.saveFoodItemPhosphorus = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemPhosphorus", [options]); };
 
-HKAddFood.prototype.saveFoodItemPhosphorus = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemPhosphorus", [options]; };
+HKAddFood.prototype.saveFoodItemPotassium = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemPotassium", [options]); };
 
-HKAddFood.prototype.saveFoodItemPotassium = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemPotassium", [options]; };
+HKAddFood.prototype.saveFoodItemProtein = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemProtein", [options]); };
 
-HKAddFood.prototype.saveFoodItemProtein = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemProtein", [options]; };
+HKAddFood.prototype.saveFoodItemRiboflavin = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemRiboflavin", [options]); };
 
-HKAddFood.prototype.saveFoodItemRiboflavin = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemRiboflavin", [options]; };
+HKAddFood.prototype.saveFoodItemSelenium = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemSelenium", [options]); };
 
-HKAddFood.prototype.saveFoodItemSelenium = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemSelenium", [options]; };
+HKAddFood.prototype.saveFoodItemSodium = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemSodium", [options]); };
 
-HKAddFood.prototype.saveFoodItemSodium = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemSodium", [options]; };
+HKAddFood.prototype.saveFoodItemSugar = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemSugar", [options]); };
 
-HKAddFood.prototype.saveFoodItemSugar = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemSugar", [options]; };
+HKAddFood.prototype.saveFoodItemThiamin = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemThiamin", [options]); };
 
-HKAddFood.prototype.saveFoodItemThiamin = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemThiamin", [options]; };
+HKAddFood.prototype.saveFoodItemVitaminA = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminA", [options]); };
 
-HKAddFood.prototype.saveFoodItemVitaminA = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminA", [options]; };
+HKAddFood.prototype.saveFoodItemVitaminB12 = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminB12", [options]); };
 
-HKAddFood.prototype.saveFoodItemVitaminB12 = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminB12", [options]; };
+HKAddFood.prototype.saveFoodItemVitaminB6 = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminB6", [options]); };
 
-HKAddFood.prototype.saveFoodItemVitaminB6 = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminB6", [options]; };
+HKAddFood.prototype.saveFoodItemVitaminC = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminC", [options]); };
 
-HKAddFood.prototype.saveFoodItemVitaminC = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminC", [options]; };
+HKAddFood.prototype.saveFoodItemVitaminD = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminD", [options]); };
 
-HKAddFood.prototype.saveFoodItemVitaminD = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminD", [options]; };
+HKAddFood.prototype.saveFoodItemVitaminE = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminE", [options]); };
 
-HKAddFood.prototype.saveFoodItemVitaminE = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminE", [options]; };
+HKAddFood.prototype.saveFoodItemVitaminK = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminK", [options]); };
 
-HKAddFood.prototype.saveFoodItemVitaminK = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemVitaminK", [options]; };
-
-HKAddFood.prototype.saveFoodItemZinc = function options, successCallback, errorCallback { cordova.execsuccessCallback, errorCallback, "HKAddFood", "saveFoodItemZinc", [options]; };
+HKAddFood.prototype.saveFoodItemZinc = function (options, successCallback, errorCallback) { cordova.exec(successCallback, errorCallback, "HKAddFood", "saveFoodItemZinc", [options]); };
 
 
 window.hkaddfood = new HKAddFood();
